abb850dc512e44f0a22a3396329ea561bae3905f
Kartikpatel231 <kartikpatel1950@gmail.com>
2022-09-21 07:25:28 +0530

    Login With Google is Complete

---
+++ src/main/java/com/mycompany/studentmanagementapp/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.mycompany.studentmanagementapp.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StudentController {
+    @GetMapping("/greet")
+    public RedirectView greet(){
+        RedirectView redirectView=new RedirectView();
+       // redirectView.setUrl("http://localhost:63342/student-management-app/static/home.html?_ijt=pqtjk2v4kj71b204f73g82sds8");
+        redirectView.setUrl("http://localhost:9000/home.html");
+        return redirectView;
+    //  return "/";
+    }
+    @GetMapping("/current-user-principal")
+    public Principal currentUser(Principal principal){
+        return principal;
+    }
+
+    @GetMapping("/current-user-details")
+    public Map<String,Object> authUser(@AuthenticationPrincipal OAuth2User user){
+        return user.getAttributes();
+    }
+}
---
+++ src/main/java/com/mycompany/studentmanagementapp/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.mycompany.studentmanagementapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .antMatchers("/index.html").permitAll()//except index.html do google authentication for all other urls
+                .anyRequest().authenticated()
+                .and().oauth2Login();
+    }
+}
